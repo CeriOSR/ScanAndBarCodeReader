21868854d728e3c1b77f4d575a12038ee3767455
Rey Cerio <rey.cerio@appetiser.com.au>
2019-11-25 09:43:04 +0800

    Initial Commit

---
+++ QRandBarCodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRandBarCodeReader
+//
+//  Created by Rey Cerio on 2019-11-25.
+//  Copyright © 2019 Rey Cerio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
